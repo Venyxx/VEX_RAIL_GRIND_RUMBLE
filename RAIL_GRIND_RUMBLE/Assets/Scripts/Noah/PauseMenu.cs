4d0c5d13ae2b700d3498abf9fac9a8800a3cd751
NoahLam15 <noah.lam@knights.ucf.edu>
2022-10-17 23:06:24 -0400

    Pause Menu
    Updated framework for Pause Menu. Still some bugs to work out but button layout and some functionality complete.

--- RAIL_GRIND_RUMBLE/Assets/Scripts/Noah/PauseMenu.cs
+++ RAIL_GRIND_RUMBLE/Assets/Scripts/Noah/PauseMenu.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+
+    public string mainMenuScene;
+    public string pauseSettings;
+    public GameObject pauseMenu;
+    public bool isPaused;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,47 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("p"))
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+                Cursor.visible = false;
+              
+            }
+            else
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
     }
+
+    public void PauseSettings()
+    { 
+    
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
