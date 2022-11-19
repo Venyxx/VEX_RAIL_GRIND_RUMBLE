d4ecb2403ca11712eef3fb14e419a01a805e45ef
Venyxx <scottvanessa01@gmail.com>
2022-11-19 18:50:30 -0500

    Railv4 Startup

---
+++ RAIL_GRIND_RUMBLE/Assets/Scripts/Vanessa/Rails.V4/PlayerRailCollider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRailCollider : MonoBehaviour
+{
+    private ThirdPersonMovement ThirdPersonMovementREF;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThirdPersonMovementREF = gameObject.GetComponent<ThirdPersonMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.name == "PositiveRunner")
+        {
+            gameObject.transform.position = Vector3.Lerp (gameObject.transform.position, col.gameObject.transform.position, 0.1f);
+            gameObject.transform.parent = col.transform;
+            col.gameObject.GetComponent<PositiveRunner>().SpeedAdjustment(ThirdPersonMovementREF.moveSpeed);
+        }
+    }
+}
---
+++ RAIL_GRIND_RUMBLE/Assets/Scripts/Vanessa/Rails.V4/PositiveRunner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+    public class PositiveRunner : MonoBehaviour
+    {
+        public PathCreator pathCreator;
+        private VertexPath vertexPath;
+        public EndOfPathInstruction endOfPathInstruction;
+        public float speed = 40;
+        [SerializeField] public float distanceTravelled;
+
+            void Start() 
+            {
+                
+                if (pathCreator != null)
+                {
+                    // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                    pathCreator.pathUpdated += OnPathChanged;
+                }
+            }
+
+            void Update()
+            {
+                
+                if (pathCreator != null)
+                {
+                    distanceTravelled += speed * Time.deltaTime;
+                    transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
+                    transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                }
+            }
+
+            // If the path changes during the game, update the distance travelled so that the follower's position on the new path
+            // is as close as possible to its position on the old path
+            void OnPathChanged() 
+            {
+                distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+            }
+
+            public void SpeedAdjustment (float speedPass)
+            {
+                speed = speedPass;
+            }
+    }
