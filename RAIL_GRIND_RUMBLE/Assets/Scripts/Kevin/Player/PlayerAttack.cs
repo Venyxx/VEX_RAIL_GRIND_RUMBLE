4183b03a8bcbdecc92b5dcb829f1d777a24f26b4
Obama00 <112011874+Obama00@users.noreply.github.com>
2022-11-30 12:54:25 -0500

    just a double check

--- RAIL_GRIND_RUMBLE/Assets/Scripts/Kevin/Player/PlayerAttack.cs
+++ RAIL_GRIND_RUMBLE/Assets/Scripts/Kevin/Player/PlayerAttack.cs
@@ -6,16 +6,20 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
     public Animator anim;
-    public int Damage = 100;
+    public int Damage;
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
     public bool IsAttacking = false;
     public static PlayerAttack instance;
+    public Collider Weapon; 
+
 
     private void Awake()
     {
         instance = this;
+        Weapon.enabled = false;
+
     }
    
     void Start()
@@ -23,20 +27,19 @@ public class PlayerAttack : MonoBehaviour
         anim = GetComponent<Animator>();
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-       
+      
     }
+   
 
     public void Attack(InputAction.CallbackContext context)
-    {  Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+    {  //Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
         if (!context.started)
         {
             return;
         }
-       // anim.SetTrigger("Attack");
+       
 
         if (context.started && !IsAttacking)
         {
@@ -45,23 +48,32 @@ public class PlayerAttack : MonoBehaviour
     
 
 
-       foreach(Collider enemy in hitEnemies)
-       {
-             IDamageable damageable;
-        if (enemy.TryGetComponent<IDamageable>(out damageable))
+   //    foreach(Collider enemy in hitEnemies)
+     //  {
+       //      IDamageable damageable;
+       // if (enemy.TryGetComponent<IDamageable>(out damageable))
+       // {
+       //     damageable.TakeDamage(Damage);
+        //}
+       //}
+    }
+
+private void OnTriggerEnter(Collider other)
+    {
+            IDamageable damageable;
+        if (other.TryGetComponent<IDamageable>(out damageable))
         {
             damageable.TakeDamage(Damage);
         }
-       }
     }
 
-void OnDrawGizmosSelected()
-{
-    if (attackPoint == null)
-    return;
+//void OnDrawGizmosSelected()
+//{
+ //   if (attackPoint == null)
+  //  return;
 
-    Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-}
+  //  Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+//}
 
 
 }
