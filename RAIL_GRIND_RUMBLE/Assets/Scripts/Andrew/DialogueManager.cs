a6ccb9d764bfd00a6e19668c680bee262d569f75
Spyktail <77474476+Spyktail@users.noreply.github.com>
2022-12-04 16:48:26 -0500

    dialogueSystem updates

--- RAIL_GRIND_RUMBLE/Assets/Scripts/Andrew/DialogueManager.cs
+++ RAIL_GRIND_RUMBLE/Assets/Scripts/Andrew/DialogueManager.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
-{
+{ 
+    public TextMeshProUGUI textComponent;
+    public TextMeshProUGUI talkingToName;
+    public GameObject dialogueBox;
     private Queue<string> paragraphDisplayed;
 
     // Start is called before the first frame update
     void Start()
     {
         paragraphDisplayed = new Queue<string>();
-
+        textComponent.text = string.Empty;
+        talkingToName.text = string.Empty;
+        dialogueBox.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,13 +27,61 @@ public class DialogueManager : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter()
-    {
-        Debug.Log("trigger");
-    }
-
     public void StartDialogue(DialogueTemplate dialogue)
     {
-        Debug.Log("Starting dialogue");
+        dialogueBox.SetActive(true);
+        talkingToName.text = dialogue.name;
+
+        paragraphDisplayed.Clear();
+
+        foreach (string paragraph in dialogue.paragraphs)
+        {
+            paragraphDisplayed.Enqueue(paragraph);
+        }
+
+        DisplayNextParagraph();
     }
+
+    public void DialogueInput(InputAction.CallbackContext context)
+    {
+        DisplayNextParagraph();
+
+    }
+
+    public void DisplayNextParagraph ()
+    {
+        if (paragraphDisplayed.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string paragraph = paragraphDisplayed.Dequeue();
+        textComponent.text = paragraph;
+    }
+
+    void EndDialogue()
+    {
+        //dialogueBox.SetActive(false);
+    }
+
+
+    /* FROM THE ORIGINAL DIALOGUEBOX SCRIPT THAT PETE HELPED ME CHANGE
+     * 
+     public void DialogueInput(InputAction.CallbackContext context)
+    {
+        if(!context.started) return;
+
+        if (textComponent.text == lines[conversationIndex])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textComponent.text = lines[conversationIndex];
+        }
+    
+    }
+     */
 }
