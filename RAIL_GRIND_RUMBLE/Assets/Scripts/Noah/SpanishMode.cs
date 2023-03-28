431efe21630f95fcb00fffcb79d73ca9224d77cf
Venyxx <scottvanessa01@gmail.com>
2023-03-28 00:56:40 -0400

    Revert "Merge remote-tracking branch 'origin/NL-Spanish'"
    This reverts commit 673b30c26bd6eba6c8922a62dbbfc4df94049992, reversing
    changes made to ecca25156b3cb9ed075144d07bad5d400798b8d5.

--- RAIL_GRIND_RUMBLE/Assets/Scripts/Noah/SpanishMode.cs
+++ RAIL_GRIND_RUMBLE/Assets/Scripts/Noah/SpanishMode.cs
@@ -10,42 +10,15 @@ public class SpanishMode : MonoBehaviour
     public static bool spanishMode = false;
     [SerializeField] private Toggle SpanishTog = null;
 
-    private GameObject[] UITexts;
-    public List<GameObject> englishUI = new List<GameObject>();
-    public List<GameObject> spanishUI = new List<GameObject>();
-
     void Start()
     {
-        UITexts = FindObjectsOfType<GameObject>(true);
-        for (int i = 0; i < UITexts.Length; i++)
-        {
-            if (UITexts[i].tag == "EnglishUI")
-            {
-                englishUI.Add(UITexts[i]);
-            }
-            if (UITexts[i].tag == "SpanishUI")
-            {
-                spanishUI.Add(UITexts[i]);
-            }
-        }
-        
     
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (spanishMode == true)
-        {
-            SpanishTog.GetComponent<Toggle>().isOn = true;
-            ActivateSpanishMode();
-        }
-        else
-        {
-            SpanishTog.GetComponent<Toggle>().isOn = false;
-            DeactivateSpanishMode();
-        }
-        
+    
     }
 
     public void SpanishToggle()
@@ -53,43 +26,12 @@ public class SpanishMode : MonoBehaviour
         if (SpanishTog.GetComponent<Toggle>().isOn == false)
         {
             spanishMode = false;
-            DeactivateSpanishMode();
             Debug.Log("It's not spanish time");
-            
         }
         else
         {
             spanishMode = true;
-            ActivateSpanishMode();
             Debug.Log("Es hora de Espanol");
-        }
-    }
-
-    public void ActivateSpanishMode()
-    {
-        for (int i = 0; i < englishUI.Count; i++)
-        {
-            englishUI[i].SetActive(false);
-        }
-
-        for (int i = 0; i < spanishUI.Count; i++)
-        {
-            spanishUI[i].SetActive(true);
-
-        }
-    }
-
-    public void DeactivateSpanishMode()
-    {
-        for (int i = 0; i < englishUI.Count; i++)
-        {
-            englishUI[i].SetActive(true);
-        }
-
-        for (int i = 0; i < spanishUI.Count; i++)
-        {
-            spanishUI[i].SetActive(false);
-
         }
     }
 }
