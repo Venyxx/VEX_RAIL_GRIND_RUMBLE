6c030d87bd65b16c09cccd73a8c770ec694d24a5
Venyxx <scottvanessa01@gmail.com>
2023-04-17 00:17:10 -0400

    Knockdown

--- RAIL_GRIND_RUMBLE/Assets/AIControl.cs
+++ RAIL_GRIND_RUMBLE/Assets/AIControl.cs
@@ -5,17 +5,23 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-     GameObject [] goalLocations;
+    public GameObject [] goalLocations;
     NavMeshAgent agent;
     Animator anim;
     private AudioSource audioSource;
     public AudioClip AudioClip;
+    private bool isEvading;
+    private float maxTime = 2;
+    private float currentTime;
+
+    private float hitSpeed = 1;
 
     void Start() {
 
+        
         audioSource = this.GetComponent<AudioSource>();
         agent = this.GetComponent<NavMeshAgent>();
-        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        goalLocations = GameObject.FindGameObjectsWithTag("walkingGoal");
         int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
         anim = this.GetComponent<Animator>();
@@ -23,7 +29,7 @@ public class AIControl : MonoBehaviour {
         anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
         float sm = Random.Range(0.5f,2);
         anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
+        agent.speed *= (sm * hitSpeed);
 
     }
 
@@ -36,6 +42,16 @@ public class AIControl : MonoBehaviour {
             agent.SetDestination(goalLocations[i].transform.position);
         }
 
+        if (currentTime > 0 && isEvading)
+            hitSpeed = 0;
+        else
+        {
+            isEvading = false;
+            hitSpeed = 1;
+            currentTime -= Time.time;
+        }
+            
+
 
     }
 
@@ -43,6 +59,11 @@ public class AIControl : MonoBehaviour {
     {
         if (col.gameObject.name == "playerPrefab")
         {
+            anim.SetTrigger("evade");
+            isEvading = true;
+            currentTime = maxTime;
+            Debug.Log("fall time");
+
             float sound = Random.Range(0,5);
 
             if (sound < 1)
