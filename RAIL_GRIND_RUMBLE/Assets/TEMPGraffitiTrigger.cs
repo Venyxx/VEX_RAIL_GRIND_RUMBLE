7036f18d374aefc20356f0947248fdbbd5d26116
Venyxx <scottvanessa01@gmail.com>
2023-04-18 02:12:41 -0400

    clean

--- RAIL_GRIND_RUMBLE/Assets/TEMPGraffitiTrigger.cs
+++ RAIL_GRIND_RUMBLE/Assets/TEMPGraffitiTrigger.cs
@@ -16,6 +16,7 @@ public class TEMPGraffitiTrigger : MonoBehaviour
                         countQuest.IncrementCount();
                         countQuest.IncrementCount();
                     }
+     Destroy(gameObject);
      }
    }
 }
